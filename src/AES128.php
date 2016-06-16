3b8d72caacc4bd6a51961704d40454ce900f5aee
Damian Gładys <damian.gladys.cracow@gmail.com>
2016-06-16 12:17:36 +0200

    Poprawienie działania algorytmu

--- src/AES128.php
+++ src/AES128.php
@@ -14,9 +14,13 @@ class AES128
      * @param string $key Key necessary to encrypt data
      * @return string
      */
-    public static function encrypt($data, $key = "testkey", $mode = MCRYPT_MODE_CBC)
+    public static function encrypt($data, $key = "bcb04b7e103a0cd", $mode = MCRYPT_MODE_CBC)
     {
-        return mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $data, $mode);
+        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, $mode);
+        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+
+        $key = $key."\0";
+        return base64_encode($iv.mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $data, $mode, $iv));
     }
 
     /**
@@ -25,8 +29,15 @@ class AES128
      * @param string $key
      * @return string
      */
-    public static function decrypt($encryptedData, $key = "testkey", $mode = MCRYPT_MODE_CBC)
+    public static function decrypt($encryptedData, $key = "bcb04b7e103a0cd", $mode = MCRYPT_MODE_CBC)
     {
-        return mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $encryptedData, $mode);
+        $iv_size =  mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, $mode);
+        $ciphertext_dec = base64_decode($encryptedData);
+        $iv_dec = substr($ciphertext_dec, 0, $iv_size);
+        $ciphertext_dec = substr($ciphertext_dec, $iv_size);
+
+
+        $key = $key."\0";
+        return mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $ciphertext_dec, $mode, $iv_dec);
     }
 }
